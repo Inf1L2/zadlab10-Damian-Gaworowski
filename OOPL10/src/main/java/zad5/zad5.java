cfc8bc219f370b18d226cdd9e653228070c2b242
HP <HP@DESKTOP-LSMEN80>
2018-05-20 23:33:05 +0200

    12.03.2018

---
+++ OOPL10/src/main/java/zad5/zad5.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zad5;
+
+
+
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class zad5{
+  
+        
+  
+  public static void zapisz(){
+	  Scanner input = new Scanner(System.in);
+	  System.out.println("Podaj swoje imię");
+	  String imie = input.nextLine();
+try{
+	  PrintWriter zapis = new PrintWriter("imie.txt");
+	  zapis.print(imie);
+	  zapis.close();
+          System.out.println("wpisano");
+          
+}
+catch(IOException e){
+    System.out.println("bład IOException ");
+}
+  }
+public static void odczytaj() {
+try{
+	  Scanner odczyt = new Scanner(new File("imie.txt"));
+	  System.out.println(odczyt.nextLine());
+}
+          catch(FileNotFoundException e){
+            System.out.println("Plik nie istnieje!");
+        } catch(IOException e){
+            System.out.println("błąd IOException");
+        }
+          
+}
+public static void main(String[] args){
+              zapisz();
+              odczytaj();
+          }
+}
+
+
+
+
+
+ 
